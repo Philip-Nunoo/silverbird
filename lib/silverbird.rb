4886231a698e8cf36ff895c258f005282d3aaa15
Philip A. Nunoo <ketay99@gmail.com>
2014-12-12 19:32:21 +0000

    no image problem

--- lib/silverbird.rb
+++ lib/silverbird.rb
@@ -5,32 +5,33 @@ require 'nokogiri'
 module Silverbird
 # Your code goes here...
 
-	def self.movies_in city  	
-		movies = Array.new
-		url = "http://silverbirdcinemas.com/#{city.to_s.downcase}"
+def self.movies_in city  	
+	movies = Array.new
+	url = "http://silverbirdcinemas.com/#{city.to_s.downcase}"
 
-		doc = Nokogiri::HTML(open(url))
-		doc.css(".catItemIsFeatured").each do |item|
+	doc = Nokogiri::HTML(open(url))
+	doc.css(".catItemIsFeatured").each do |item|
 			# puts "here"
 			movie = Movie.new
 			title = item.at_css(".catItemTitle a").text
 			running_time = item.at_css('.catItemExtraFieldsValue').text
-			image_url = nil
+			image_url = "http://cdn.ghanaweb.com/imagelib/pics/65028362.jpg"
 
 			intro_text = "No description available"
 			movie.intro_text = intro_text
-			
+
 			if item.at_css('.catItemIntroText p')!=nil 
 				intro_text = item.at_css('.catItemIntroText p').text
 				movie.intro_text = intro_text.strip
 			end
 
+			movie.image_url = image_url
 			if(item.at_css('.catItemImageBlock img') != nil)
 				image_url = item.at_css('.catItemImageBlock img')[:src]
+				movie.image_url = "http://silverbirdcinemas.com/#{image_url}"
 			end
 			movie.title = title.strip
 			movie.time = running_time.strip
-			movie.image_url = "http://silverbirdcinemas.com/#{image_url}"
 
 			# movie.title item.at_css(".catItemTitle a").text
 			movies.push movie
@@ -46,7 +47,7 @@ end
 
 class City
 	def initialize(name)
-	  @name = name
+		@name = name
 	end
 end
 
